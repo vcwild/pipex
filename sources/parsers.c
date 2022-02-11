9c474ea5b5b1b8ede5422090b1cf8dd340b7a1c7
Victor Wildner <vwildner@redhat.com>
2022-02-11 01:28:51 -0300

    feat: add parsers

---
+++ sources/parsers.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsers.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/10 23:08:29 by vwildner          #+#    #+#             */
+/*   Updated: 2022/02/11 01:24:54 by vwildner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+static	char *remove_args(char *str, char *args)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		k = -1;
+		while (args[++k])
+		{
+			if (str[i] == args[k])
+			{
+				printf("removed: %c\n", str[i]);
+				i++;
+			}
+			else
+				str[j] = str[i];
+		}
+		i++;
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
+}
+
+static char	**split_command(char *arg)
+{
+	char	**args;
+	char	*tmp;
+
+	tmp = ft_strtrim(arg, " ");
+	tmp = remove_args(tmp, "\'\\\"");
+	args = ft_split(tmp, ' ');
+	free(tmp);
+	return (args);
+}
+
+int	parse_args(char **argv, t_pipex *pipex)
+{
+	int i;
+	int j;
+
+	i = -1;
+	while (++i < pipex->ncmd)
+		pipex->cmds[i] = split_command(argv[i + 2]);
+	// TODO: remove this logging code
+	printf("`parse_args` Success!\n");
+	i = -1;
+	while (pipex->cmds[++i])
+	{
+		j = -1;
+		while (pipex->cmds[i][++j])
+			printf("cmds[%i][%i]: %s\n", i, j, pipex->cmds[i][j]);
+	}
+	// End of logging code
+	return (0);
+}
+
+int	parse_env_path(char **envp, t_pipex *pipex)
+{
+	char	*path_str;
+
+	path_str = NULL;
+	while (*envp)
+	{
+		if (ft_strncmp("PATH", *envp, 4) == 0)
+		{
+			path_str = ft_strdup(*envp + 5);
+			printf("`parse_env_path` Success!\n");
+		}
+		envp++;
+	}
+	if (path_str == NULL)
+		handle_exit("`parse_env_path`: PATH variable not found", 5, pipex);
+	pipex->envp = ft_split(path_str, ':');
+	free(path_str);
+	// TODO: remove this logging message
+	int i = -1;
+	while (pipex->envp[++i])
+	{
+		printf("pipex->envp[%i] = '%s'\n", i, pipex->envp[i]);
+	}
+	// End of logging message
+	return (0);
+}

869a42c0e0ef9d560e47767b1054d15f6660fe89
Victor Wildner <vwildner@redhat.com>
2022-02-11 01:28:59 -0300

    feat: add getters

---
+++ sources/getters.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   getters.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/09 09:56:10 by vwildner          #+#    #+#             */
+/*   Updated: 2022/02/11 01:22:50 by vwildner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	get_args(int argc, char **argv)
+{
+	int	i;
+
+	i = -1;
+	if (argc < 5)
+	{
+		perror("check_args usage: ./pipex <cmd1> <cmd2> <cmd3> <cmd4>\n");
+		return (1);
+	}
+	if (access(argv[1], F_OK) != 0)
+	{
+		perror(ft_strjoin("bash: ", argv[1]));
+		return (2);
+	}
+	printf("`check_args` Success!\n");
+	while (argv[++i])
+		printf("arg[%i]: %s\n", i, argv[i]);
+	return (0);
+}

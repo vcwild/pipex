039fdb9a40d348433ec4b6a71739cce48d50ac2c
vcwild <vcwildm@gmail.com>
2022-02-11 22:47:25 -0300

    feat: create memory handling functions

--- sources/free.c
+++ sources/free.c
@@ -6,7 +6,7 @@
 /*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 20:09:20 by vwildner          #+#    #+#             */
-/*   Updated: 2022/02/11 01:25:45 by vwildner         ###   ########.fr       */
+/*   Updated: 2022/02/11 22:25:39 by vwildner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,16 +45,32 @@ void	free_ptr(void **ptr)
 	}
 }
 
-void	free_all(t_pipex *pipex)
+void	free_all(t_pipex *self)
 {
 	int i;
 
 	i = -1;
-	while (pipex->cmds[++i])
-		free_matrix((void *)&pipex->cmds[i]);
-	free_ptr((void *)&pipex->cmds);
-	i = -1;
-	free_str(pipex->envp);
+	while (self->args[++i])
+		free_matrix((void *)&self->args[i]);
+	free_ptr((void *)&self->args);
+	free_str(self->envp);
+	if (self->pipes)
+		free_matrix((void *)&self->pipes);
 
-	free_ptr((void *)&pipex->cmd_path);
+	free_ptr((void *)&self->cmd_path);
+}
+
+void	free_cmd(t_pipex *self, int cmd_pos)
+{
+	int		len;
+	char	*err_msg;
+
+	err_msg = ft_strjoin(self->args[cmd_pos][0],
+			": command not found\n");
+	len = ft_strlen(err_msg);
+	cmd_pos = write(2, err_msg, len);
+    free(err_msg);
+	free_all(self);
+	exit(127);
+	return ;
 }

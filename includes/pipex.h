0dbde552a3518328beca34f976515235171c561e
vcwild <vcwildm@gmail.com>
2022-02-11 22:46:51 -0300

    fix: update pipex header

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 19:47:17 by vwildner          #+#    #+#             */
-/*   Updated: 2022/02/11 12:11:21 by vwildner         ###   ########.fr       */
+/*   Updated: 2022/02/11 22:42:47 by vwildner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,34 +28,40 @@
 
 typedef struct s_pipex
 {
+	char			***args;
+	char			**cmd_path;
+	char			**envp;
 	char			*input_path;
 	char			*output_path;
-	char			**args;
-	char			**envp;
-	char			***cmds;
+	int				**pipes;
 	int				argc;
-	int				ncmd;
-	int				npipes;
-	int				here_doc;
-	char			***cmd_path;
 	int				fd[2];
-	int				status;
+	int				here_doc;
+	int				nargs;
+	int				npipes;
 }					t_pipex;
 
 void	free_str(char **str);
 void	free_matrix(void ***mat);
 void	free_ptr(void **ptr);
+void	free_cmd(t_pipex *self, int cmd_pos);
+void	free_all(t_pipex *self);
 
 int		get_args(int argc, char **argv);
-int		set_mem_alloc(t_pipex *pipex);
-int		set_references(int argc, char **argv, t_pipex *pipex);
 
-int		parse_args(char **argv, t_pipex *pipex);
-int		parse_env_path(char **envp, t_pipex *pipex);
+int		set_mem_alloc(t_pipex *self);
+int		set_references(t_pipex *self, int argc, char **argv);
+int		set_input_fd(t_pipex *self);
+int		set_output_fd(t_pipex *self);
+int		set_abspath(t_pipex *self, int cmd_pos);
 
-void	free_all(t_pipex *pipex);
+int		parse_args(t_pipex *self, char **argv);
+int		parse_env_path(t_pipex *self, char **envp);
 
 void	handle_exit(char *err_msg, int pipex_errno, t_pipex *pipex);
 
+void	run_input_cmd(t_pipex *self, char **envp);
+void	run_iter_cmd(t_pipex *self, char **envp, int i);
+void	run_output_cmd(t_pipex *self, char **envp);
 
 #endif
